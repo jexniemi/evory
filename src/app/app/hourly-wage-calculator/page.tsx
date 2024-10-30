3c869192c5b3d856beb72ce989c259dfa4570e59
Jeremi Nieminen <jeremi.nieminen@gmail.com>
2024-10-30 14:59:59 +0200

    Fix text

--- src/app/app/hourly-wage-calculator/page.tsx
+++ src/app/app/hourly-wage-calculator/page.tsx
@@ -16,7 +16,7 @@ const pageProps = {
   seoTitle: "Hourly Wage Calculator - Easily Calculate Your Hourly Wage",
   description:
     "Do you know how to convert a monthly salary into an hourly wage? With the hourly wage calculator, you can easily convert your monthly salary into an hourly wage.",
-  instructions: `To convert a monthly salary into an hourly wage, divide the monthly salary by the divisor specified in the employment contract, or based on the average monthly work hours. An average full-time employee (working 38.25 hours per week) typically works around 166 hours per month. Enter your monthly salary and your monthly work hours, and the calculator will determine your hourly wage. You can use either gross or net salary for the calculation.`,
+  instructions: `To convert a monthly salary into an hourly wage, divide the monthly salary by the divisor specified in the employment contract, or based on the average monthly work hours. An average full-time employee (working 40 hours per week) typically works around 173 hours per month. Enter your monthly salary and your monthly work hours, and the calculator will determine your hourly wage. You can use either gross or net salary for the calculation.`,
   Info,
 };
 
