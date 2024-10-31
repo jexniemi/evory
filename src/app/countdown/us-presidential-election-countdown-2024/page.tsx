12eea8ff5992af4a1e4c0a0d49d9849cc25adeaa
Jeremi Nieminen <jeremi.nieminen@gmail.com>
2024-10-31 17:51:40 +0200

    fixes

--- src/app/countdown/us-presidential-election-countdown-2024/page.tsx
+++ src/app/countdown/us-presidential-election-countdown-2024/page.tsx
@@ -20,7 +20,7 @@ const pageProps = {
     "2024 Presidential Election Countdown - See how many days are left until Election Day.",
   description:
     "How many days until the 2024 Presidential Election? Track the countdown to Election Day in real-time using our handy countdown tool. Watch the days, hours, and minutes decrease as we approach this pivotal moment in democracy. Get informed and prepare to make your voice heard!",
-  instructions: `Welcome to the Presidential Election countdown! Excitedly track the decreasing days, hours, and minutes until the crucial Election Day on November 5, 2024. Set your start date, and the countdown will show you in real-time how much time is left until you can cast your vote. Start counting down the days now and prepare to participate in shaping the future! Additionally, you can embed this on Countdown on your website.`,
+  instructions: `Welcome to the Presidential Election countdown! Excitedly track the decreasing days, hours, and minutes until the crucial Election Day on November 5, 2024. Set your start date, and the countdown will show you in real-time how much time is left until you can cast your vote. Start counting down the days now and prepare to participate in shaping the future!`,
   Info,
 };
 
