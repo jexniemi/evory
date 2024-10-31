890ccdf03f4f3b1f6a4685d73dcc0c650be70f92
Jeremi Nieminen <jeremi.nieminen@gmail.com>
2024-10-31 12:57:10 +0200

    Better countdown

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -33,11 +33,13 @@ const Countdown: React.FC<CountdownProps> = ({
     <div>
       <div>
         <h2 className="text-xl">
-          {targetDateName}{" "}
-          {target.toLocaleDateString().replace("/", ".").replace("/", ".")}{" "}
+          <b>
+            {targetDateName}{" "}
+            {target.toLocaleDateString().replace("/", ".").replace("/", ".")}{" "}
+          </b>
           countdown:
         </h2>
-        <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+        <div className="grid grid-flow-col gap-5 text-center auto-cols-max mt-5">
           <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
             <span className="countdown font-mono text-5xl">
               <span style={{ "--value": days }}></span>
