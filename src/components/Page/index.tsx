0ec79196bdae6df055444e067de60885b4b27a96
Jeremi Nieminen <jeremi.nieminen@gmail.com>
2026-03-31 18:05:50 +0300

    asd

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -57,7 +57,9 @@ export default function Page({
         {Info && (
           <>
             <hr className="my-12 h-0.5 border-t-0 bg-neutral-100 opacity-100" />
-            <Info />
+            <div className="prose prose-lg max-w-none">
+              <Info />
+            </div>
             <div className="my-12" />
           </>
         )}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
 import daisyui from "daisyui";
+import typography from "@tailwindcss/typography";
 
 const config: Config = {
   content: [
@@ -26,7 +27,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [daisyui],
+  plugins: [daisyui, typography],
   daisyui: {
     logs: false,
   },
